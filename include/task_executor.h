317fb18ce96bd20f9e97e125888b6bea4c585bb2
Kyurnepoto <heal9179@gmail.com>
2019-07-11 17:41:15 +0900

    Design architecture of library

--- include/task_executor.h
+++ include/task_executor.h
@@ -2,15 +2,40 @@
 
 namespace task_executor
 {
-    inline namespace detail
+    inline namespace interface_v1
     {
         template<class Executor, class Callable>
-        typename Executor::result_type post(Executor &&, Callable &&);
+        typename Executor::result_type<Callable> post(Executor &&, Callable &&);
 
         template<class Executor, class Callable>
-        typename Executor::result_type dispatch(Executor&&, Callable&&);
+        typename Executor::result_type<Callable> dispatch(Executor &&, Callable &&);
 
         template<class Executor, class Callable>
-        typename Executor::result_type defer(Executor&&, Callable&&);
+        typename Executor::result_type<Callable> defer(Executor &&, Callable &&);
+    }
+
+    inline namespace executor_v1
+    {
+        struct system;
+        
+        struct thread_pool;
+        
+        struct strand;
+    }
+
+    inline namespace async_executor_v1
+    {
+        struct async_executor;
+        
+        template<class Owner>
+        struct async_executor_impl;
+        
+        template<class Owner>
+        struct local_async_executor : async_executor_impl<Owner>;
+        
+        template<class Owner>
+        struct global_async_executor : async_executor_impl<Owner>;
+        
+        struct async_executor_distributor;
     }
 }
