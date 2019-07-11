51266be959b5d488128b9c8a9bf4067bea685fc7
Kyurnepoto <heal9179@gmail.com>
2019-07-11 17:49:17 +0900

    Fix architecture

--- include/task_executor.h
+++ include/task_executor.h
@@ -4,38 +4,38 @@ namespace task_executor
 {
     inline namespace interface_v1
     {
-        template<class Executor, class Callable>
-        typename Executor::result_type<Callable> post(Executor &&, Callable &&);
+        template<class Executor, class Executable>
+        typename Executor::result_type<Executable> post(Executor &&, Executable &&);
 
-        template<class Executor, class Callable>
-        typename Executor::result_type<Callable> dispatch(Executor &&, Callable &&);
+        template<class Executor, class Executable>
+        typename Executor::result_type<Executable> dispatch(Executor &&, Executable &&);
 
-        template<class Executor, class Callable>
-        typename Executor::result_type<Callable> defer(Executor &&, Callable &&);
+        template<class Executor, class Executable>
+        typename Executor::result_type<Executable> defer(Executor &&, Executable &&);
     }
 
     inline namespace executor_v1
     {
-        struct system;
-        
-        struct thread_pool;
-        
-        struct strand;
-    }
+        inline namespace async_executor_v1
+        {
+            struct async_executor;
 
-    inline namespace async_executor_v1
-    {
-        struct async_executor;
-        
-        template<class Owner>
-        struct async_executor_impl;
-        
-        template<class Owner>
-        struct local_async_executor : async_executor_impl<Owner>;
-        
-        template<class Owner>
-        struct global_async_executor : async_executor_impl<Owner>;
-        
-        struct async_executor_distributor;
+            template<class Owner>
+            struct async_executor_impl;
+
+            template<class Owner>
+            struct local_async_executor : async_executor_impl<Owner>;
+
+            template<class Owner>
+            struct global_async_executor : async_executor_impl<Owner>;
+
+            struct async_executor_distributor;
+        }
+
+        struct system;
+
+        struct thread_pool;
+
+        struct strand;
     }
 }
