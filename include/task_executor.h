d1a45e79fedac5cceca66afa16c3ac6fa9ffc87e
Kyurenpoto <heal9179@gmail.com>
2019-07-12 17:22:10 +0900

    Fix architecture

--- include/task_executor.h
+++ include/task_executor.h
@@ -4,14 +4,29 @@ namespace task_executor
 {
     inline namespace interface_v1
     {
-        template<class Executor, class Executable>
-        typename Executor::result_type<Executable> post(Executor &&, Executable &&);
+        template<class...>
+        struct executor_result_t;
 
         template<class Executor, class Executable>
-        typename Executor::result_type<Executable> dispatch(Executor &&, Executable &&);
+        executor_result_t<Executor, Executable> post(Executor &&, Executable &&);
 
         template<class Executor, class Executable>
-        typename Executor::result_type<Executable> defer(Executor &&, Executable &&);
+        executor_result_t<Executor, Executable> dispatch(Executor &&, Executable &&);
+
+        template<class Executor, class Executable>
+        executor_result_t<Executor, Executable> defer(Executor &&, Executable &&);
+
+        template<class...>
+        struct awaiter_result_t;
+
+        template<class Awaiter, class Awaitable>
+        awaiter_result_t<Awaiter, Awaitable> post(Awaiter &&, Awaitable &&);
+
+        template<class Awaiter, class Awaitable>
+        awaiter_result_t<Awaiter, Awaitable> dispatch(Awaiter &&, Awaitable &&);
+
+        template<class Awaiter, class Awaitable>
+        awaiter_result_t<Awaiter, Awaitable> defer(Awaiter &&, Awaitable &&);
     }
 
     inline namespace executor_v1
@@ -21,13 +36,10 @@ namespace task_executor
             struct async_executor;
 
             template<class Owner>
-            struct async_executor_impl;
+            struct local_async_executor;
 
             template<class Owner>
-            struct local_async_executor : async_executor_impl<Owner>;
-
-            template<class Owner>
-            struct global_async_executor : async_executor_impl<Owner>;
+            struct global_async_executor;
 
             struct async_executor_distributor;
         }
@@ -37,5 +49,10 @@ namespace task_executor
         struct thread_pool;
 
         struct strand;
+    }
+
+    inline namespace awaiter_v1
+    {
+
     }
 }
