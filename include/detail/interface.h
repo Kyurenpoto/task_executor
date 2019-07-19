47d9cd4d8517b82c0b7ea8fc7c326f100238153d
Kyurnepoto <heal9179@gmail.com>
2019-07-20 02:08:19 +0900

    Remove unnecessary interfaces

--- include/detail/interface.h
+++ include/detail/interface.h
@@ -1,90 +1,177 @@
 #pragma once
 
+#include "detail/traits.h"
+
 namespace task_executor
 {
     inline namespace interface_v1
     {
-        struct op_base
+        template<class Executor, class... Executable>
+        auto post(Executor && executor, Executable && ... executable)
         {
-            struct op_raw;
-            struct op_continuation;
-            struct op_wrap;
-        };
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
 
-        struct op_execute : op_base
+            return executor.post(executable...);
+        }
+
+        template<class Executor, class... Executable>
+        auto dispatch(Executor && executor, Executable && ... executable)
         {
-            template<class T0, class... T>
-            static constexpr bool is_op_raw =
-                is_executor_v<T0> &&
-                sizeof...(T) > 0 &&
-                is_executable_v<T>...;
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
 
-            template<class T0, class... T>
-            static constexpr bool is_op_continuation =
-                is_executor_v<T0> &&
-                sizeof...(T) == 1 &&
-                is_executable_continuation_v<T>...;
+            return executor.dispatch(executable...);
+        }
 
-            template<class T0, class... T>
-            static constexpr bool is_op_wrap =
-                is_wraped_executable_continuation_v<T0> &&
-                is_wraped_executable_continuation_v<T>...;
-
-            template<class... T>
-            using type =
-                std::conditional_t<is_op_raw<T...>, op_raw,
-                std::conditional_t<is_op_continuation<T...>, op_continuation,
-                std::conditional_t<is_op_wrap<T...>, op_wrap, void>>>;
-        };
-
-        struct op_await : op_base
+        template<class Executor, class... Executable>
+        auto defer(Executor && executor, Executable && ... executable)
         {
-            template<class T0, class... T>
-            static constexpr bool is_op_raw =
-                is_awaiter_v<T0> &&
-                sizeof...(T) > 0 &&
-                is_awaitable_v<T>...;
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
 
-            template<class T0, class... T>
-            static constexpr bool is_op_continuation =
-                is_awaiter_v<T0> &&
-                sizeof...(T) == 1 &&
-                is_awaitable_continuation_v<T>...;
+            return executor.defer(executable...);
+        }
 
-            template<class T0, class... T>
-            static constexpr bool is_op_wrap =
-                is_wraped_awaitable_continuation_v<T0> &&
-                is_wraped_awaitable_continuation_v<T>...;
+        template<class Executor, class Duration, class... Executable>
+        auto post_after(Executor && executor, Duration && duration, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
 
-            template<class... T>
-            using type =
-                std::conditional_t<is_op_raw<T...>, op_raw,
-                std::conditional_t<is_op_continuation<T...>, op_continuation,
-                std::conditional_t<is_op_wrap<T...>, op_wrap, void>>>;
-        };
+            return executor.post_after(duration, executable...);
+        }
+
+        template<class Executor, class Duration, class... Executable>
+        auto dispatch_after(Executor && executor, Duration && duration, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.dispatch_after(duration, executable...);
+        }
+
+        template<class Executor, class Duration, class... Executable>
+        auto defer_after(Executor && executor, Duration && duration, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.defer_after(duration, executable...);
+        }
+
+        template<class Executor, class TimePoint, class... Executable>
+        auto post_at(Executor && executor, TimePoint && timepoint, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.post_at(timepoint, executable...);
+        }
+
+        template<class Executor, class TimePoint, class... Executable>
+        auto dispatch_at(Executor && executor, TimePoint && timepoint, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.dispatch_at(timepoint, executable...);
+        }
+
+        template<class Executor, class TimePoint, class... Executable>
+        auto defer_at(Executor && executor, TimePoint && timepoint, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.defer_at(timepoint, executable...);
+        }
     }
 }
 
-#include "interface/post.h"
-#include "interface/dispatch.h"
-#include "interface/defer.h"
+namespace task_executor
+{
+    inline namespace interface_v1
+    {
+        template<class Awaiter, class... Awaitable>
+        auto co_post(Awaiter && awaiter, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
 
-#include "interface/post_after.h"
-#include "interface/dispatch_after.h"
-#include "interface/defer_after.h"
+            return awaiter.co_post(awaitable...);
+        }
 
-#include "interface/post_at.h"
-#include "interface/dispatch_at.h"
-#include "interface/defer_at.h"
+        template<class Awaiter, class... Awaitable>
+        auto co_dispatch(Awaiter && awaiter, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
 
-#include "interface/co_post.h"
-#include "interface/co_dispatch.h"
-#include "interface/co_defer.h"
+            return awaiter.co_dispatch(awaitable...);
+        }
 
-#include "interface/co_post_after.h"
-#include "interface/co_dispatch_after.h"
-#include "interface/co_defer_after.h"
+        template<class Awaiter, class... Awaitable>
+        auto co_defer(Awaiter && awaiter, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
 
-#include "interface/co_post_at.h"
-#include "interface/co_dispatch_at.h"
-#include "interface/co_defer_at.h"
+            return awaiter.co_defer(awaitable...);
+        }
+
+        template<class Awaiter, class Duration, class... Awaitable>
+        auto co_post_after(Awaiter && awaiter, Duration && duration, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.co_post_after(duration, awaitable...);
+        }
+
+        template<class Awaiter, class Duration, class... Awaitable>
+        auto co_dispatch_after(Awaiter && awaiter, Duration && duration, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.co_dispatch_after(duration, awaitable...);
+        }
+
+        template<class Awaiter, class Duration, class... Awaitable>
+        auto co_defer_after(Awaiter && awaiter, Duration && duration, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.co_defer_after(duration, awaitable...);
+        }
+
+        template<class Awaiter, class TimePoint, class... Awaitable>
+        auto co_post_at(Awaiter && awaiter, TimePoint && timepoint, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.co_post_at(timepoint, awaitable...);
+        }
+
+        template<class Awaiter, class TimePoint, class... Awaitable>
+        auto co_dispatch_at(Awaiter && awaiter, TimePoint && timepoint, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.co_dispatch_at(timepoint, awaitable...);
+        }
+
+        template<class Awaiter, class TimePoint, class... Awaitable>
+        auto co_defer_at(Awaiter && awaiter, TimePoint && timepoint, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.co_defer_at(timepoint, awaitable...);
+        }
+    }
+}
--- include/task_executor.h
+++ include/task_executor.h
@@ -2,79 +2,34 @@
 
 namespace task_executor
 {
-    template<class...>
-    struct execution_result;
-
-    template<class... T>
-    using execution_result_t =
-        typename execution_result<T...>::type;
-
-    template<class...>
-    struct await_result;
-
-    template<class... T>
-    using await_result_t =
-        typename await_result<T...>::type;
-
-    template<class, class>
-    struct executable_continuation_wrap;
-
-    template<class T, class U>
-    using executable_continuation_wrap_t =
-        typename executable_continuation_wrap<T, U>::type;
-    
-    template<class, class>
-    struct awaitable_continuation_wrap;
-
-    template<class T, class U>
-    using awaitable_continuation_wrap_t =
-        typename awaitable_continuation_wrap<T, U>::type;
-
-    template<class...>
-    struct executable_continuation;
-
-    template<class... T>
-    using executable_continuation_t =
-        typename executable_continuation<T...>::type;
-
-    template<class...>
-    struct awaitable_continuation;
-
-    template<class... T>
-    using awaitable_continuation_t =
-        typename awaitable_continuation<T...>::type;
-}
-
-namespace task_executor
-{
     inline namespace interface_v1
     {
-        template<class... T>
-        auto post(T && ... args);
+        template<class Executor, class... Executable>
+        auto post(Executor && executor, Executable && ... executable);
 
-        template<class... T>
-        auto dispatch(T && ... args);
+        template<class Executor, class... Executable>
+        auto dispatch(Executor && executor, Executable && ... executable);
 
-        template<class... T>
-        auto defer(T && ... args);
+        template<class Executor, class... Executable>
+        auto defer(Executor && executor, Executable && ... executable);
 
-        template<class... T>
-        auto post_after(T && ... args);
+        template<class Executor, class Duration, class... Executable>
+        auto post_after(Executor && executor, Duration && duration, Executable && ... executable);
 
-        template<class... T>
-        auto dispatch_after(T && ... args);
+        template<class Executor, class Duration, class... Executable>
+        auto dispatch_after(Executor && executor, Duration && duration, Executable && ... executable);
 
-        template<class... T>
-        auto defer_after(T && ... args);
+        template<class Executor, class Duration, class... Executable>
+        auto defer_after(Executor && executor, Duration && duration, Executable && ... executable);
 
-        template<class... T>
-        auto post_at(T && ... args);
+        template<class Executor, class TimePoint, class... Executable>
+        auto post_at(Executor && executor, TimePoint && timepoint, Executable && ... executable);
 
-        template<class... T>
-        auto dispatch_at(T && ... args);
+        template<class Executor, class TimePoint, class... Executable>
+        auto dispatch_at(Executor && executor, TimePoint && timepoint, Executable && ... executable);
 
-        template<class... T>
-        auto defer_at(T && ... args);
+        template<class Executor, class TimePoint, class... Executable>
+        auto defer_at(Executor && executor, TimePoint && timepoint, Executable && ... executable);
     }
 }
 
@@ -82,32 +37,32 @@ namespace task_executor
 {
     inline namespace interface_v1
     {
-        template<class... T>
-        auto co_post(T && ... args);
+        template<class Awaiter, class... Awaitable>
+        auto co_post(Awaiter && awaiter, Awaitable && ... awaitable);
 
-        template<class... T>
-        auto co_dispatch(T && ... args);
+        template<class Awaiter, class... Awaitable>
+        auto co_dispatch(Awaiter && awaiter, Awaitable && ... awaitable);
 
-        template<class... T>
-        auto co_defer(T && ... args);
+        template<class Awaiter, class... Awaitable>
+        auto co_defer(Awaiter && awaiter, Awaitable && ... awaitable);
 
-        template<class... T>
-        auto co_post_after(T && ... args);
+        template<class Awaiter, class Duration, class... Awaitable>
+        auto co_post_after(Awaiter && awaiter, Duration && duration, Awaitable && ... awaitable);
 
-        template<class... T>
-        auto co_dispatch_after(T && ... args);
+        template<class Awaiter, class Duration, class... Awaitable>
+        auto co_dispatch_after(Awaiter && awaiter, Duration && duration, Awaitable && ... awaitable);
 
-        template<class... T>
-        auto co_defer_after(T && ... args);
+        template<class Awaiter, class Duration, class... Awaitable>
+        auto co_defer_after(Awaiter && awaiter, Duration && duration, Awaitable && ... awaitable);
 
-        template<class... T>
-        auto co_post_at(T && ... args);
+        template<class Awaiter, class TimePoint, class... Awaitable>
+        auto co_post_at(Awaiter && awaiter, TimePoint && timepoint, Awaitable && ... awaitable);
 
-        template<class... T>
-        auto co_dispatch_at(T && ... args);
+        template<class Awaiter, class TimePoint, class... Awaitable>
+        auto co_dispatch_at(Awaiter && awaiter, TimePoint && timepoint, Awaitable && ... awaitable);
 
-        template<class... T>
-        auto co_defer_at(T && ... args);
+        template<class Awaiter, class TimePoint, class... Awaitable>
+        auto co_defer_at(Awaiter && awaiter, TimePoint && timepoint, Awaitable && ... awaitable);
     }
 }
 
