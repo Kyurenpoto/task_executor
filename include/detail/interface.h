78beb450ab291b05f0fbcecbf49e20cf384cacf3
Kyurnepoto <heal9179@gmail.com>
2019-07-20 03:25:21 +0900

    Add concurrent interfaces

--- include/detail/interface.h
+++ include/detail/interface.h
@@ -175,3 +175,177 @@ namespace task_executor
         }
     }
 }
+
+namespace task_executor
+{
+    inline namespace interface_v1
+    {
+        template<class Executor, class... Executable>
+        auto crt_post(Executor && executor, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.crt_post(executable...);
+        }
+
+        template<class Executor, class... Executable>
+        auto crt_dispatch(Executor && executor, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.crt_dispatch(executable...);
+        }
+
+        template<class Executor, class... Executable>
+        auto crt_defer(Executor && executor, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.crt_defer(executable...);
+        }
+
+        template<class Executor, class Duration, class... Executable>
+        auto crt_post_after(Executor && executor, Duration && duration, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.crt_post_after(duration, executable...);
+        }
+
+        template<class Executor, class Duration, class... Executable>
+        auto crt_dispatch_after(Executor && executor, Duration && duration, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.crt_dispatch_after(duration, executable...);
+        }
+
+        template<class Executor, class Duration, class... Executable>
+        auto crt_defer_after(Executor && executor, Duration && duration, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.crt_defer_after(duration, executable...);
+        }
+
+        template<class Executor, class TimePoint, class... Executable>
+        auto crt_post_at(Executor && executor, TimePoint && timepoint, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.crt_post_at(timepoint, executable...);
+        }
+
+        template<class Executor, class TimePoint, class... Executable>
+        auto crt_dispatch_at(Executor && executor, TimePoint && timepoint, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.crt_dispatch_at(timepoint, executable...);
+        }
+
+        template<class Executor, class TimePoint, class... Executable>
+        auto crt_defer_at(Executor && executor, TimePoint && timepoint, Executable && ... executable)
+        {
+            static_assert(is_executor_v<Executor> && is_executable_v<Executable>...,
+                "It doesn't satisfy executor traits or executable traits");
+
+            return executor.crt_defer_at(timepoint, executable...);
+        }
+    }
+}
+
+namespace task_executor
+{
+    inline namespace interface_v1
+    {
+        template<class Awaiter, class... Awaitable>
+        auto crt_co_post(Awaiter && awaiter, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.crt_co_post(awaitable...);
+        }
+
+        template<class Awaiter, class... Awaitable>
+        auto crt_co_dispatch(Awaiter && awaiter, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.crt_co_dispatch(awaitable...);
+        }
+
+        template<class Awaiter, class... Awaitable>
+        auto crt_co_defer(Awaiter && awaiter, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.crt_co_defer(awaitable...);
+        }
+
+        template<class Awaiter, class Duration, class... Awaitable>
+        auto crt_co_post_after(Awaiter && awaiter, Duration && duration, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.crt_co_post_after(duration, awaitable...);
+        }
+
+        template<class Awaiter, class Duration, class... Awaitable>
+        auto crt_co_dispatch_after(Awaiter && awaiter, Duration && duration, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.crt_co_dispatch_after(duration, awaitable...);
+        }
+
+        template<class Awaiter, class Duration, class... Awaitable>
+        auto crt_co_defer_after(Awaiter && awaiter, Duration && duration, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.crt_co_defer_after(duration, awaitable...);
+        }
+
+        template<class Awaiter, class TimePoint, class... Awaitable>
+        auto crt_co_post_at(Awaiter && awaiter, TimePoint && timepoint, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.crt_co_post_at(timepoint, awaitable...);
+        }
+
+        template<class Awaiter, class TimePoint, class... Awaitable>
+        auto crt_co_dispatch_at(Awaiter && awaiter, TimePoint && timepoint, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.crt_co_dispatch_at(timepoint, awaitable...);
+        }
+
+        template<class Awaiter, class TimePoint, class... Awaitable>
+        auto crt_co_defer_at(Awaiter && awaiter, TimePoint && timepoint, Awaitable && ... awaitable)
+        {
+            static_assert(is_awaiter_v<Awaiter> && is_awaitable_v<Awaitable>...,
+                "It doesn't satisfy awaiter traits or awaitable traits");
+
+            return awaiter.crt_co_defer_at(timepoint, awaitable...);
+        }
+    }
+}
--- include/task_executor.h
+++ include/task_executor.h
@@ -68,6 +68,72 @@ namespace task_executor
 
 namespace task_executor
 {
+    inline namespace interface_v1
+    {
+        template<class Executor, class... Executable>
+        auto crt_post(Executor && executor, Executable && ... executable);
+
+        template<class Executor, class... Executable>
+        auto crt_dispatch(Executor && executor, Executable && ... executable);
+
+        template<class Executor, class... Executable>
+        auto crt_defer(Executor && executor, Executable && ... executable);
+
+        template<class Executor, class Duration, class... Executable>
+        auto crt_post_after(Executor && executor, Duration && duration, Executable && ... executable);
+
+        template<class Executor, class Duration, class... Executable>
+        auto crt_dispatch_after(Executor && executor, Duration && duration, Executable && ... executable);
+
+        template<class Executor, class Duration, class... Executable>
+        auto crt_defer_after(Executor && executor, Duration && duration, Executable && ... executable);
+
+        template<class Executor, class TimePoint, class... Executable>
+        auto crt_post_at(Executor && executor, TimePoint && timepoint, Executable && ... executable);
+
+        template<class Executor, class TimePoint, class... Executable>
+        auto crt_dispatch_at(Executor && executor, TimePoint && timepoint, Executable && ... executable);
+
+        template<class Executor, class TimePoint, class... Executable>
+        auto crt_defer_at(Executor && executor, TimePoint && timepoint, Executable && ... executable);
+    }
+}
+
+namespace task_executor
+{
+    inline namespace interface_v1
+    {
+        template<class Awaiter, class... Awaitable>
+        auto crt_co_post(Awaiter && awaiter, Awaitable && ... awaitable);
+
+        template<class Awaiter, class... Awaitable>
+        auto crt_co_dispatch(Awaiter && awaiter, Awaitable && ... awaitable);
+
+        template<class Awaiter, class... Awaitable>
+        auto crt_co_defer(Awaiter && awaiter, Awaitable && ... awaitable);
+
+        template<class Awaiter, class Duration, class... Awaitable>
+        auto crt_co_post_after(Awaiter && awaiter, Duration && duration, Awaitable && ... awaitable);
+
+        template<class Awaiter, class Duration, class... Awaitable>
+        auto crt_co_dispatch_after(Awaiter && awaiter, Duration && duration, Awaitable && ... awaitable);
+
+        template<class Awaiter, class Duration, class... Awaitable>
+        auto crt_co_defer_after(Awaiter && awaiter, Duration && duration, Awaitable && ... awaitable);
+
+        template<class Awaiter, class TimePoint, class... Awaitable>
+        auto crt_co_post_at(Awaiter && awaiter, TimePoint && timepoint, Awaitable && ... awaitable);
+
+        template<class Awaiter, class TimePoint, class... Awaitable>
+        auto crt_co_dispatch_at(Awaiter && awaiter, TimePoint && timepoint, Awaitable && ... awaitable);
+
+        template<class Awaiter, class TimePoint, class... Awaitable>
+        auto crt_co_defer_at(Awaiter && awaiter, TimePoint && timepoint, Awaitable && ... awaitable);
+    }
+}
+
+namespace task_executor
+{
     inline namespace executor_v1
     {
         struct executor;
