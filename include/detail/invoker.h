1e8e3a1c12b35096438b5296610a0df3b5787ba2
Kyurenpoto <heal9179@gmail.com>
2019-07-24 18:19:42 +0900

    Change design of invoker

--- include/detail/invoker.h
+++ include/detail/invoker.h
@@ -2,6 +2,8 @@
 
 #include <future>
 #include <tuple>
+#include <algorithm>
+#include <utility>
 
 #include "traits.h"
 #include "callable.h"
@@ -10,61 +12,44 @@ namespace task_executor
 {
     inline namespace invoker_v1
     {
-        enum class invoker_t
+        namespace detail
         {
-            seq_exe,
-            seq_awt,
-            par_exe,
-            par_awt
-        };
+            template<class... T, std::size_t... I>
+            auto get_futures_impl(std::tuple<std::promise<T>...> promises, std::index_sequence<I...>)
+            {
+                return std::make_tuple(std::get<I>(promises).get_future());
+            }
 
-        template<invoker_t, class... T>
-        struct invoker;
+            template<class... T>
+            auto get_futures(std::tuple<std::promise<T>...> promises)
+            {
+                return get_futures_impl(promises, std::make_index_sequence<sizeof...(T)>{});
+            }
+        }
 
         template<class... Executable>
-        struct invoker<invoker_t::seq_exe, Executable...>
+        struct executable_invoker
         {
-            invoker(Executable && ...);
+            executable_invoker(Executable && ... executables) :
+                executables{ executables }
+            {}
 
-            void invoke();
-            auto get_future();
+            auto get_futures()
+            {
+                return detail::get_futures(promises);
+            }
 
-        private:
             template<class T>
             using return_type = typename function_traits<T>::return_type;
 
-            template<class T>
-            using invoker_type = invoker<invoker_t::seq_exe, T>;
-
-            size_t idx;
-            size_t cntComplete;
-            std::promise<std::tuple<return_type<Executable>...>> promise;
-            std::tuple<std::future<return_type<Executable>>...> futures;
-            std::tuple<invoker_type<Executable>...> invokers;
+            std::tuple<callable<Executable>...> executables;
+            std::tuple<std::promise<return_type<Executable>>...> promises;
         };
 
-        template<class Executable>
-        struct invoker<invoker_t::seq_exe, Executable>
+        template<class... Awaitable>
+        struct awaitable_invoker
         {
-            invoker(Executable &&);
-
-            void invoke();
-            auto get_future();
-
-        private:
-            using return_type = typename function_traits<Executable>::return_type;
-
-            std::promise<return_type> promise;
-            callable<Executable> executable;
+            awaitable_invoker();
         };
-
-        template<class... Awaitable>
-        struct invoker<invoker_t::seq_awt, Awaitable...>;
-
-        template<class... Executable>
-        struct invoker<invoker_t::par_exe, Executable...>;
-
-        template<class... Awaitable>
-        struct invoker<invoker_t::par_awt, Awaitable...>;
     }
 }
